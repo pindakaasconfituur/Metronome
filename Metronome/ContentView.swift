70ae87b737d641e5232c6bf800a459f4abb58240
Cem Demirayak <cemdemirayak@Cems-MacBook-Air.local>
2023-07-24 15:04:46 +0200

    first commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi");
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Cem Demirayak on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
