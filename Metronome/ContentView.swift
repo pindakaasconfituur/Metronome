e6d56be144675f8e48dca6c167cb23b3dfa002d7
Cem Demirayak <cemdemirayak@Cems-MacBook-Air.local>
2023-07-25 20:38:34 +0200

    removed bar

--- Metronome/ContentView.swift
+++ Metronome/ContentView.swift
@@ -3,7 +3,6 @@ import AVFAudio
 
 struct ContentView: View {
     
-    @State private var bar = 2.0
     @State private var isEditingBar = false
     @State private var tempo = 60.0
     @State private var isEditingTempo = false
@@ -30,16 +29,6 @@ struct ContentView: View {
                 
                 Spacer();
                 
-                Text("Bar");
-                
-                HStack{
-                    Slider(value: $bar, in: 0...4,step: 1.0, onEditingChanged: {
-                        editing in isEditingBar = editing
-                    }
-                    ).disabled(true);
-                    Text("\(Int(bar))")
-                        .foregroundColor(isEditingBar ? .blue : .black)
-                }
                 Text("Tempo");
                 HStack{
                     Slider(value: $tempo, in: 0...240,step: 1.0, onEditingChanged: {
